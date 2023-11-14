ac07640aeca5802340ae9a0a14e7793aec088593
WMatheus2022 <pachwander4@gmail.com>
2023-11-14 11:18:12 -0300

    Exercicios ImaTech

---
+++ IMA-training/Logica-algoritimos/script.js
@@ -0,0 +1,18 @@
+const classHum = document.querySelector('.primeira');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //console.log('hi, how are you? are you from this planet?');
+    //document.write('hi, how are you? are you from this planet?');
